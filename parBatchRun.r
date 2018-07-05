844faf31eeaa07630e194e9d65f33ba3d4698eb9
sdnjohnson <samueldnjohnson@gmail.com>
2018-07-04 23:25:31 -0700

    Updated

--- parBatchRun.r
+++ parBatchRun.r
@@ -2,7 +2,7 @@ library(parallel)
 # set number of batch files to use
 # nBatchFiles <- 30
 # batchFiles <- 1:8
-batchFiles <- c(1:18)
+batchFiles <- c(1:12)
 nBatchFiles <- length(batchFiles)
 batchFolderNames <- paste("mseRBat",batchFiles,sep = "")
 
